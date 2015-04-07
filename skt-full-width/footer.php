3038f0d2526fa276475e4a2daa263b5f1e9c9968
Gateway FurMeet website(s) <gfm@li418-45.members.linode.com>
2015-04-06 21:37:24 -0500

    piwik

--- skt-full-width/footer.php
+++ skt-full-width/footer.php
@@ -62,5 +62,20 @@
   </div><!-- wrapper -->
 
 <?php wp_footer(); ?>
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(["trackPageView"]);
+  _paq.push(["enableLinkTracking"]);
+
+  (function() {
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://stats.missourifurs.com/";
+    _paq.push(["setTrackerUrl", u+"piwik.php"]);
+    _paq.push(["setSiteId", "5"]);
+    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Piwik Code -->
 </body>
 </html>
