112296d2f83dd2c9f7651544faefa385571c57e4
Alec Elton <alec.elton@gmail.com>
2015-11-15 17:33:36 +0000

    piwik is insecure, remove it

--- skt-full-width/footer.php
+++ skt-full-width/footer.php
@@ -62,21 +62,6 @@
   </div><!-- wrapper -->
 
 <?php wp_footer(); ?>
-<!-- Piwik -->
-<script type="text/javascript">
-  var _paq = _paq || [];
-  _paq.push(["trackPageView"]);
-  _paq.push(["enableLinkTracking"]);
-
-  (function() {
-    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://stats.missourifurs.com/";
-    _paq.push(["setTrackerUrl", u+"piwik.php"]);
-    _paq.push(["setSiteId", "5"]);
-    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
-    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<!-- End Piwik Code -->
 <!-- ga - gfm gapps account -->
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
